55dcd0116afb1a52b8056dbfcf24c2192a22b064
Lord Kakabel <50675941+LordKakabel@users.noreply.github.com>
2024-07-30 17:38:35 -0400

    Swing Sword
    Player stops moving while swinging sword.

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -3,9 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-	[SerializeField] float _speed = 5.0f;
+    [SerializeField] float _speed = 5.0f;
+    [SerializeField] float _swordSwingTime = 1.0f;
+    [SerializeField] GameObject _sword;
+
+    bool _canMove = true;
+    WaitForSeconds _swordSwingYield;
+
+    private void Awake() {
+        _swordSwingYield = new WaitForSeconds(_swordSwingTime);
+    }
+
+    private void Update() {
+        if (_canMove && Input.GetKeyDown(KeyCode.Space)) {
+            _canMove = false;
+            _sword.SetActive(true);
+            StartCoroutine(SwingSword());
+        }
+    }
 
     private void FixedUpdate() {
+        if (_canMove) {
+            HandleMovement();
+        }
+    }
+
+    private void HandleMovement() {
         Vector3 move = new(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         transform.position += _speed * Time.deltaTime * move;
         Vector3 direction = move.normalized;
@@ -13,5 +36,11 @@ public class PlayerMovement : MonoBehaviour {
         if (move != Vector3.zero) {
             transform.up = direction;
         }
+    }
+
+    private IEnumerator SwingSword() {
+        yield return _swordSwingYield;
+        _sword.SetActive(false);
+        _canMove = true;
     }
 }
