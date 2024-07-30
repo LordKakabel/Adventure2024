0a4e24b9acbf6dc28752b10a65b0afa93d2fde87
Lord Kakabel <50675941+LordKakabel@users.noreply.github.com>
2024-07-30 19:56:40 -0400

    Kill Enemy
    The player's weapon can kill enemies.

---
+++ Assets/_Scripts/Enemy.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.GetComponentInChildren<PlayerWeapon>() != null) {
+            Destroy(gameObject);
+        }
+    }
+}
