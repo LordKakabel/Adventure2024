85aea6a7f30bfede5a06040ce3b213e75a96d8ac
Lord Kakabel <50675941+LordKakabel@users.noreply.github.com>
2024-09-01 21:00:58 -0400

    Drop Item Bug
    Fixed a bug where player could not drop an item in the enemy's detection radius. Still cannot drop an item on top of an enemy (i.e. if the enemy is directly behind the player), but that is fine--a feature, not a bug. It could be corrected by giving the enemy's hitbox its own layer and having the PlayerItemHitbox ignore that layer.

--- Assets/_Scripts/PlayerHitBox.cs
+++ Assets/_Scripts/PlayerHitBox.cs
@@ -3,59 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHitBox : MonoBehaviour {
-    [SerializeField] PlayerMovement _playerMovement;
-    [SerializeField] GameObject _itemPrefab;
-    [SerializeField] float _droppedItemAreaRadius = (0.75f / 2);
-    
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.Q)) {
-            DropItem();
-        }
-    }
-
     public void Damage() {
         Debug.Log("Damage!");
         //Destroy(transform.parent.gameObject);
         //UIManager.Instance.GameOverScreen();
-    }
-
-    private bool DropItem() {
-        // If there IS an item to be dropped,
-        if (ItemUI.Instance.IsHoldingItem()) {
-
-            // Look for a free area behind the player
-            Vector2 dropPoint = transform.position - _playerMovement.GetDirection();
-            Collider2D result = Physics2D.OverlapCircle(dropPoint, _droppedItemAreaRadius);
-
-            // If there IS a free area (no colliders were found), drop it
-            if (!result) {
-                ItemSO droppedItemSO = ItemUI.Instance.DropItem();
-                GameObject droppedItem = Instantiate(_itemPrefab, dropPoint, Quaternion.identity);
-                droppedItem.GetComponent<Item>().CreateItem(droppedItemSO);
-
-                return true;
-            }
-
-            return false;
-        }
-
-        return true;
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision) {
-        // If there IS an item,
-        Item item = collision.gameObject.GetComponent<Item>();
-        if (item) {
-            // If we ARE already holding an item,
-            if (ItemUI.Instance.IsHoldingItem()) {
-                // If we can NOT drop it, return
-                if (!DropItem())
-                    return;
-            }
-
-            // Pickup the item
-            ItemUI.Instance.SetItem(item.ItemSO);
-            Destroy(collision.gameObject);
-        }
     }
 }
---
+++ Assets/_Scripts/PlayerItemHitbox.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItemHitbox : MonoBehaviour {
+    [SerializeField] PlayerMovement _playerMovement;
+    [SerializeField] GameObject _itemPrefab;
+    [SerializeField] float _droppedItemAreaRadius = (0.75f / 2);
+    [SerializeField] LayerMask _enemyLayerMask;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Q)) {
+            DropItem();
+        }
+    }
+
+    private bool DropItem() {
+        // If there IS an item to be dropped,
+        if (ItemUI.Instance.IsHoldingItem()) {
+
+            // Look for a free area behind the player
+            Vector2 dropPoint = transform.position - _playerMovement.GetDirection();
+            Collider2D result = Physics2D.OverlapCircle(dropPoint, _droppedItemAreaRadius, _enemyLayerMask);
+
+            // If there IS a free area (no colliders were found), drop it
+            if (!result) {
+                ItemSO droppedItemSO = ItemUI.Instance.DropItem();
+                GameObject droppedItem = Instantiate(_itemPrefab, dropPoint, Quaternion.identity);
+                droppedItem.GetComponent<Item>().CreateItem(droppedItemSO);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        return true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log(collision.gameObject.ToString());
+        // If there IS an item,
+        Item item = collision.gameObject.GetComponent<Item>();
+        if (item) {
+            // If we ARE already holding an item,
+            if (ItemUI.Instance.IsHoldingItem()) {
+                // If we can NOT drop it, return
+                if (!DropItem())
+                    return;
+            }
+
+            // Pickup the item
+            ItemUI.Instance.SetItem(item.ItemSO);
+            Destroy(collision.gameObject);
+        }
+    }
+}
